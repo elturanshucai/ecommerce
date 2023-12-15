da0952797cd085e46ed96601600d45fafc69a6ca
elturanshucai <elturanfcb@gmail.com>
2023-12-15 02:00:16 -0800

    hero bottom completed

---
+++ src/components/hero/HeroBottom.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import images from '../../images'
+
+export default function HeroBottom() {
+    return (
+        <div className='relative z-10 flex justify-center mb-44'>
+            <div className='absolute -top-14 flex flex-col sm:flex-row items-center justify-center gap-7'>
+                <img src={images.heroWoman} alt="woman" className='max-w-[390px] w-4/5 mx-auto sm:w-[30%]' />
+                <img src={images.heroMan} alt="man" className='max-w-[390px] w-4/5 mx-auto sm:w-[30%]' />
+                <img src={images.heroKid} alt="kid" className='max-w-[390px] w-4/5 mx-auto sm:w-[30%]' />
+            </div>
+        </div>
+    )
+}
