758a5fb445a9356b830fa449eb40f1aae5bda038
elturanshucai <elturanfcb@gmail.com>
2023-12-25 03:51:48 -0800

    pagination component completed

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
-export default function Pagination() {
-  return (
-    <div>Pagination</div>
-  )
+export default function Pagination({
+    totalDataLength = 4,
+    setShowCount,
+    setSorting,
+    defaultShow,
+    activePage,
+    setActivePage
+}) {
+    const [pageCount, setPageCount] = useState(1)
+
+    const handleClick = (e) => {
+        let number = Number(e.target.value)
+        setActivePage(number)
+    }
+
+    useEffect(() => {
+        setPageCount(Math.ceil(totalDataLength / defaultShow))
+    }, [totalDataLength, defaultShow])
+
+    useEffect(() => {
+        let pageButtons = document.querySelectorAll('#paginatebuttons button')
+        for (let i = 0; i < pageButtons.length; i++) {
+            if (activePage + 1 == pageButtons[i].innerHTML && pageButtons[i].id != 'defaultmax') {
+                pageButtons[i].classList.add('text-primary')
+            } else {
+                pageButtons[i].classList.remove('text-primary')
+            }
+        }
+    }, [activePage])
+
+    return (
+        <div>
+            <div className='flex gap-14'>
+                <div>
+                    <label className='font-bold'>Sort by</label>
+                    <select
+                        onChange={e => setSorting(e.target.value)}
+                        className='p-2 border rounded-sm ml-3 outline-none'>
+                        <option value="priceplus">Price to low</option>
+                        <option value="priceminus">Price to high</option>
+                    </select>
+                </div>
+                <div>
+                    <label className='font-bold'>Show</label>
+                    <input
+                        type="number"
+                        min={1}
+                        className='text-center p-2 border rounded-sm ml-3 outline-none w-20'
+                        onChange={e => setShowCount(e.target.value)}
+                        value={defaultShow}
+                        onWheel={e => e.target.blur()}
+                    />
+                </div>
+                <div className='flex items-center gap-4' id='paginatebuttons'>
+                    {activePage != 0 && <FaArrowLeftLong
+                        className='cursor-pointer'
+                        onClick={() => setActivePage(activePage - 1)}
+                    />}
+                    {activePage == 0 ?
+                        <>
+                            <button value={activePage} onClick={handleClick}>{activePage + 1}</button>
+                            {pageCount > 2 && <button value={activePage + 1} onClick={handleClick}>{activePage + 2}</button>}
+                            {pageCount > 3 && <button value={activePage + 2} onClick={handleClick}>{activePage + 3}</button>}
+                            {pageCount > 4 && <div>...</div>}
+                            {pageCount > 1 && <button button id='defaultmax' value={pageCount - 1} onClick={handleClick}>{pageCount}</button>}
+                        </>
+                        :
+                        //sonuncu sehife
+                        activePage + 1 == pageCount ?
+                            <>
+                                <button value={0} onClick={handleClick}>1</button>
+                                <div>...</div>
+                                <button value={activePage - 1} onClick={handleClick}>{activePage}</button>
+                                <button value={activePage} onClick={handleClick}>{activePage + 1}</button>
+                            </>
+                            :
+
+                            activePage < pageCount - 2 ?
+                                <>
+                                    <button value={activePage - 1} onClick={handleClick}>{activePage}</button>
+                                    <button value={activePage} onClick={handleClick}>{activePage + 1}</button>
+                                    {activePage < pageCount - 2 &&
+                                        <button value={activePage + 1} onClick={handleClick}>{activePage + 2}</button>
+                                    }
+                                    <div>...</div>
+                                    <button value={pageCount - 1} onClick={handleClick}>{pageCount}</button>
+                                </>
+                                :
+                                <>
+                                    <button value={activePage - 1} onClick={handleClick}>{activePage}</button>
+                                    <button value={activePage} onClick={handleClick}>{activePage + 1}</button>
+                                    <button value={pageCount - 1} onClick={handleClick}>{pageCount}</button>
+                                </>
+                    }
+                    {activePage + 1 != pageCount && <FaArrowRightLong
+                        className='cursor-pointer'
+                        onClick={() => setActivePage(activePage + 1)}
+                    />}
+                </div>
+            </div>
+        </div >
+    )
 }
--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BreadCrumbs from '../../components/BreadCrumbs'
 import MainLayout from '../../components/MainLayout'
+import Pagination from '../../components/Pagination'
+import { VscSettings } from "react-icons/vsc";
 
 export default function Catalog() {
+    const [showFilters, setShowFilters] = useState(false)
+    const [sorting, setSorting] = useState('priceplus')
+    const [showCount, setShowCount] = useState(9)
+    const [activePage, setActivePage] = useState(0)
+
+    useEffect(() => {
+
+    }, [sorting, showCount, activePage])
     return (
         <MainLayout>
             <BreadCrumbs />
             <main className='lg:px-40 px-5 py-8'>
-                
+                <div className='flex gap-14 justify-between'>
+                    <button
+                        onClick={() => setShowFilters(showFilters ? false : true)}
+                        className='flex items-center justify-center gap-2 rounded-md text-white bg-primary px-16 py-2 outline-none'>
+                        <VscSettings />
+                        {showFilters ? 'Hide filters' : 'Show filters'}
+                    </button>
+                    <Pagination
+                        setShowCount={setShowCount}
+                        setSorting={setSorting}
+                        defaultShow={showCount}
+                        activePage={activePage}
+                        setActivePage={setActivePage}
+                    />
+                </div>
             </main>
         </MainLayout>
     )
